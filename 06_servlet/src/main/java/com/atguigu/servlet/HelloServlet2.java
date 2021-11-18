a2113799cf4bbbd4d18d36a3d2b0edbc6e66c399
ZhouYao <1607435943@qq.com>
2021-11-18 21:00:23 +0800

    Initial Commit

--- 06_servlet/src/main/java/com/atguigu/servlet/HelloServlet2.java
+++ 06_servlet/src/main/java/com/atguigu/servlet/HelloServlet2.java
@@ -1,5 +1,6 @@
 package com.atguigu.servlet;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -23,6 +24,13 @@ public class HelloServlet2 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("HelloServlet2 的doGet方法");
+
+        ServletConfig servletConfig = getServletConfig();
+        System.out.println(servletConfig);
+
+        // 2、获取初始化参数init-param
+        System.out.println("初始化参数username的值是;" + servletConfig.getInitParameter("username"));
+        System.out.println("初始化参数url的值是;" + servletConfig.getInitParameter("url"));
     }
 
     /**
